7bba58ea1879e23de2f3490baa90f98fd6c522d3
Santhosh Kokkalla <santhoshlagishetti2007@gmail.com>
2025-10-28 02:34:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6943859241-260b2",
+  "appId": "1:672898826281:web:9a9cad9b9743a0cee7dc38",
+  "apiKey": "AIzaSyDryvi2kCYSuJp0MfIw8otnIJrfNjOnQeg",
+  "authDomain": "studio-6943859241-260b2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "672898826281"
+};
