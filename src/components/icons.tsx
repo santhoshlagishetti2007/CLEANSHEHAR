1c3da11c59f793946d3a6a5f612428f8405cd374
Santhosh Kokkalla <santhoshlagishetti2007@gmail.com>
2025-10-28 03:43:02 +0000

    change the logo of the website from 'Clear शहर' to the one similar to th

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,13 +12,8 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
         className="text-primary"
         {...props}
       >
-        <path
-          d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
+        <path d="M8 2.5l8 5.5v10l-8-5.5zM8 2.5L0 8l8 5.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+        <path d="M16 8l8 5.5-8 5.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
       </svg>
       <span className="text-primary">Clear</span><span className="text-accent">शहर</span>
     </div>
